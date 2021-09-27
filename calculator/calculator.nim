9ceccf47ba635833f2aa6387e9728b0572a6e56f
maxpdr96 <maxpdr96@gmail.com>
2021-09-27 17:01:43 -0300

    feat: :sparkles: add more function in calculator
    add fibonacci, potentiation and refactoring

--- calculator/calculator.nim
+++ calculator/calculator.nim
@@ -1,13 +1,24 @@
-proc sum*(x: int, y: int): int =
-    result = x + y
+import math
 
+proc calculator*(x: int, y: int, signal:string): float =
+    case signal
+    of "+":
+        return float(x + y)
+    of "-":
+        return float(x - y)
+    of "*":
+        return float(x * y)
+    of "/": 
+        return x / y
+        
+proc squareRoot*(x: float): float = 
+    return sqrt(x)
 
-proc minus*(x: int, y: int): int =
-    result = x - y
+proc potentiation*(x:float, y:float): float = 
+    return pow(x,y)
 
+proc fib*(n: uint64): uint64 =
+  if n > 2.uint64 : return fib(n - 1) + fib(n - 2)
+  return n
 
-proc multiply*(x: int, y: int): int =
-    result = x * y
-
-proc divide*(x: int, y: int): float =
-    result = x / y
+        
--- calculator/test_calculator.nim
+++ calculator/test_calculator.nim
@@ -3,13 +3,25 @@ import calculator
 
 suite "Calculator":
     test "sum 2 numbers":
-        check sum(4,2) == 6
+        check calculator(4,2,"+") == 6
 
     test "minus 2 numbers":
-        check minus(4,2) == 2
+        check calculator(4,2,"-") == 2
 
     test "multiply 2 numbers":
-        check multiply(4,2) == 8
+        check calculator(4,2,"*") == 8
 
     test "divide 2 numbers":
-        check divide(4,2) == 2
+        check calculator(4,2,"/") == 2
+
+    test "square root":
+        check squareRoot(9) == 3
+
+    test "potentiation 2":
+        check potentiation(2,2) == 4
+
+    test "potentiation -3":
+        check potentiation(2,-3) == 0.125
+
+    test "fibonacci":
+        check fib(11) == 144
