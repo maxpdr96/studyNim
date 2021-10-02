54379126d0c8159a5fa69dc4279a7aad1cec3e44
maxpdr96 <maxpdr96@gmail.com>
2021-10-02 15:20:37 -0300

    feat: add new function
    add new function

--- calculator/calculator.nim
+++ calculator/calculator.nim
@@ -1,4 +1,9 @@
-import math
+import math, strutils
+
+type
+    Latas = object
+        qtde: int
+        valor: float
 
 proc calculator*(x: int, y: int, signal:string): float =
     case signal
@@ -14,7 +19,7 @@ proc calculator*(x: int, y: int, signal:string): float =
 proc squareRoot*(x: float): float = 
     return sqrt(x)
 
-proc potentiation*(x:float, y:float): float = 
+proc potentiation*(x, y:float): float = 
     return pow(x,y)
 
 proc fib*(n: uint64): uint64 =
@@ -25,7 +30,33 @@ proc average*(x: array[4, float]):float =
     var aux = 0.0
 
     for i in low(x)..high(x):
-        aux +=  x[i]
+        aux += x[i]
     
     return aux / 4    
-        
+
+proc convertMtoCM*(x: float): float =
+    return x * 100
+
+proc areaCircle*(x: float): float =  
+    return (PI * pow(x,2)).round(2)
+
+proc fahrenheitToCelsius*(x: float): float = 
+    return 5 * ((x-32) / 9)
+
+proc celsiusToFahrenheit*(x: float): float = 
+    return (x * 9/5) + 32
+
+proc discountIR*(x: float): float =
+    return x * (11 / 100)
+
+proc discountINSS*(x: float): float =
+    return x * (8 / 100)
+
+proc discountSind*(x: float): float =
+    return x * (5 / 100)
+
+proc discountSalary(x: float): float = 
+    return discountIR(x) + discountINSS(x) + discountSind(x)
+
+proc calculateSalary*(x: float): float =
+    return x - discountSalary(x)
--- calculator/test_calculator.nim
+++ calculator/test_calculator.nim
@@ -28,3 +28,33 @@ suite "Calculator":
 
     test "average":
         check average([100.0,50.0,65.0,40.0]) == 63.75
+
+    test "convert M to Cm 1":
+        check convertMtoCM(1) == 100
+    
+    test "convert M to Cm 23.4":
+        check convertMtoCM(23.4) == 2340
+
+    test "area do circulo":
+        check areaCircle(5) == 78.54
+    
+    test "Fahrenheit To Celsius":
+        check fahrenheitToCelsius(50) == 10
+
+    test "Celsius To Fahrenheit":
+        check celsiusToFahrenheit(50) == 122
+
+    test "Celsius To Fahrenheit":
+        check celsiusToFahrenheit(50) == 122
+
+    test "Desconto Imposto de Renda":
+        check discountIR(7000) == 770
+    
+    test "Desconto INSS":
+        check discountINSS(7000) == 560
+
+    test "Desconto Sindicato":
+        check discountSind(7000) == 350
+
+    test "Salario liquido":
+        check calculateSalary(7000) == 5320
